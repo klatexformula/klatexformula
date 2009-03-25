6e1e2f7c03e780c6fba518be6b85a48064b5017b
Philippe Faist <philippe.faist@bluewin.ch>
2009-03-25 22:36:08 +0000

    top-level windows w/o parents for library and symbols. this is a test.
    git-svn-id: https://svn.code.sf.net/p/klatexformula/code/trunk/klatexformula@155 4112663c-bd99-11de-a44c-fbaffaffe1b7

--- src/klflatexsymbols.cpp
+++ src/klflatexsymbols.cpp
@@ -416,7 +416,7 @@ void KLFLatexSymbolsView::slotSymbolActivated()
 
 
 KLFLatexSymbols::KLFLatexSymbols(KLFMainWin *mw)
-  : QWidget(mw, Qt::Window), KLFLatexSymbolsUI()
+  : QWidget(/*mw*/ 0 , Qt::Window), KLFLatexSymbolsUI()
 {
   setupUi(this);
 
