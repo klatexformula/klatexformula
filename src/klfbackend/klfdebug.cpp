49a395aa979232d9456423fc973fac9acbb77f86
Philippe Faist <philippe.faist@bluewin.ch>
2010-09-21 00:51:40 +0000

    * Added missing src/klfbackend/klfdebug.cpp (!)
    git-svn-id: https://svn.code.sf.net/p/klatexformula/code/trunk/klatexformula@500 4112663c-bd99-11de-a44c-fbaffaffe1b7

---
+++ src/klfbackend/klfdebug.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+ *   file klfdebug.cpp
+ *   This file is part of the KLatexFormula Project.
+ *   Copyright (C) 2010 by Philippe Faist
+ *   philippe.faist at bluewin.ch
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+/* $Id$ */
+
+
+// All definitions are in klfdefs.cpp. I may choose to move them here in the future.
+
+// this file is here to fool automoc to create a klfdebug.moc.cpp for klfdebug.h
+// in klfkateplugin.
