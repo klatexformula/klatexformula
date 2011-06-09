5c1e65630b449e5dafff0e83ee9baa7c056d796d
Philippe Faist <philippe.faist@bluewin.ch>
2011-06-09 19:40:39 +0000

    * working on cpack for mac
    git-svn-id: https://svn.code.sf.net/p/klatexformula/code/branches/klatexformula-3.2@642 4112663c-bd99-11de-a44c-fbaffaffe1b7

--- cmake/klfcpack.cmake
+++ cmake/klfcpack.cmake
@@ -11,10 +11,10 @@ configure_file("${CMAKE_SOURCE_DIR}/cmake/welcome_installer_withlatex.txt.in"
 	       @ONLY)
 
 set(klf_package_name "klatexformula-${KLF_VERSION}-${KLF_CMAKE_OS}-${KLF_CMAKE_ARCH}")
-if(WIN32)
+if(APPLE OR WIN32)
   # ... win32-x86 is a bit redundant ...
   set(klf_package_name "klatexformula-${KLF_VERSION}-${KLF_CMAKE_OS}")
-endif(WIN32)
+endif(APPLE OR WIN32)
 
 if(KLF_INSTALL_LATEXDIST)
   set(klf_package_name "${klf_package_name}-with-latex")
@@ -36,8 +36,9 @@ set(CPACK_PACKAGE_DESCRIPTION_FILE "${CMAKE_SOURCE_DIR}/descr_long.txt")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "${CMAKE_SOURCE_DIR}/descr_short.txt")
 set(CPACK_PACKAGE_FILE_NAME "${klf_package_name}")
 set(CPACK_PACKAGE_INSTALL_DIRECTORY "KLatexFormula-${KLF_VERSION}")
-set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/COPYING")
-set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/README")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_SOURCE_DIR}/COPYING.txt")
+unset(CPACK_RESOURCE_FILE_README)
+#set(CPACK_RESOURCE_FILE_README "${CMAKE_SOURCE_DIR}/cmake/installer_readme.txt")
 set(CPACK_RESOURCE_FILE_WELCOME "${klf_welcome_installer_fnpath}")
 set(CPACK_MONOLITHIC_INSTALL TRUE)
 #set(CPACK_GENERATOR )
@@ -59,28 +60,37 @@ set(CPACK_SOURCE_IGNORE_FILES "/\\\\.svn/")
 
 # == Windows Installer ==
 
-set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "klatexformula-${KLF_VERSION}")
-set(cmake_CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/src/mswin/klficon64.ico")
-STRING(REPLACE "/" "\\\\" CPACK_NSIS_MUI_ICON "${cmake_CPACK_NSIS_MUI_ICON}")
-set(cmake_CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/src/mswin/klatexformula-nsis-header.bmp")
-STRING(REPLACE "/" "\\\\" CPACK_PACKAGE_ICON "${cmake_CPACK_PACKAGE_ICON}")
-#set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS )
-#set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS )
-#set(CPACK_NSIS_COMPRESSOR )
-set(CPACK_NSIS_MODIFY_PATH "ON")
-set(CPACK_NSIS_DISPLAY_NAME "KLatexFormula ${KLF_VERSION}")
-set(CPACK_NSIS_INSTALLED_ICON_NAME "${KLF_INSTALL_BIN_DIR}\\\\klatexformula.exe")
-set(CPACK_NSIS_HELP_LINK
+if(WIN32)
+  set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "klatexformula-${KLF_VERSION}")
+  set(cmake_CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/src/mswin/klficon64.ico")
+  STRING(REPLACE "/" "\\\\" CPACK_NSIS_MUI_ICON "${cmake_CPACK_NSIS_MUI_ICON}")
+  set(cmake_CPACK_PACKAGE_ICON "${CMAKE_SOURCE_DIR}/src/mswin/klatexformula-nsis-header.bmp")
+  STRING(REPLACE "/" "\\\\" CPACK_PACKAGE_ICON "${cmake_CPACK_PACKAGE_ICON}")
+  #set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS )
+  #set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS )
+  #set(CPACK_NSIS_COMPRESSOR )
+  set(CPACK_NSIS_MODIFY_PATH "ON")
+  set(CPACK_NSIS_DISPLAY_NAME "KLatexFormula ${KLF_VERSION}")
+  set(CPACK_NSIS_INSTALLED_ICON_NAME "${KLF_INSTALL_BIN_DIR}\\\\klatexformula.exe")
+  set(CPACK_NSIS_HELP_LINK
     "http://klatexformula.sourceforge.net/klfwiki/index.php/User_Manual:Home")
-set(CPACK_NSIS_URL_INFO_ABOUT "http://klatexformula.sourceforge.net/")
-set(CPACK_NSIS_CONTACT "philippe.faist@bluewin.ch")
-#set(CPACK_NSIS_CREATE_ICONS_EXTRA )
-#set(CPACK_NSIS_DELETE_ICONS_EXTRA )
+  set(CPACK_NSIS_URL_INFO_ABOUT "http://klatexformula.sourceforge.net/")
+  set(CPACK_NSIS_CONTACT "philippe.faist@bluewin.ch")
+  #set(CPACK_NSIS_CREATE_ICONS_EXTRA )
+  #set(CPACK_NSIS_DELETE_ICONS_EXTRA )
+endif(WIN32)
 
 
 # == Mac OS X Installer ==
-#set(CPACK_OSX_PACKAGE_VERSION )
-
+if(KLF_MACOSX_BUNDLES)
+  set(CPACK_GENERATOR "DragNDrop")
+  set(CPACK_OSX_PACKAGE_VERSION "10.5")
+  # This is used in bundle identifier...
+  set(CPACK_PACKAGE_VENDOR "klatexformula")
+  set(CPACK_DMG_VOLUME_NAME "KLatexFormula-${KLF_VERSION}")
+  set(CPACK_DMG_DS_STORE "${CMAKE_SOURCE_DIR}/cmake/DS_Store")
+  set(CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/cmake/installer_dragndrop_bg.png")
+endif(KLF_MACOSX_BUNDLES)
 
 # == Advanced ==
 #set(CPACK_CMAKE_GENERATOR )
@@ -116,7 +126,7 @@ if(KLF_USE_CPACK)
   endif(WIN32)
 
   if(KLF_MACOSX_BUNDLES)
-    KLFNote("CPack under Mac OS X is not (yet) (officially) supported.")
+    #    KLFNote("CPack under Mac OS X is not (yet) (officially) supported.")
   endif(KLF_MACOSX_BUNDLES)
   
   KLFNote("You have configured to use CPack for generating packages.
@@ -128,6 +138,8 @@ if(KLF_USE_CPACK)
 
   # include CPack stuff
   include(CPack)
+
+else(KLF_USE_CPACK)
   
 endif(KLF_USE_CPACK)
 
