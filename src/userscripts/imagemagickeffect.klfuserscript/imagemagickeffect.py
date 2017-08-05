86406d4c3279da8a9caaa509395c367585dcab6d
Philippe Faist <philippe.faist@bluewin.ch>
2017-08-05 21:34:48 +0000

    slight improvement
    git-svn-id: https://svn.code.sf.net/p/klatexformula/code/trunk/klatexformula@1041 4112663c-bd99-11de-a44c-fbaffaffe1b7

--- src/userscripts/imagemagickeffect.klfuserscript/imagemagickeffect.py
+++ src/userscripts/imagemagickeffect.klfuserscript/imagemagickeffect.py
@@ -50,14 +50,18 @@ mindim = min(width, height)
 print("Requested format is {}".format(format))
 
 
+# round() in Python2 gives a float, not an int as in Python3
+doround = lambda x : int(round(x)+0.01) # +0.01 to avoid numerical errors
+
+
 if format == 'outer-glow':
 
     factor = mindim/200.0
 
     values = dict(
-        blursize = 1+round(factor*6),
-        shiftx = 1+round(factor*4),
-        shifty = 1+round(factor*3),
+        blursize = 1+doround(factor*6),
+        shiftx = 1+doround(factor*4),
+        shifty = 1+doround(factor*3),
     )
 
     innercmd = shlex.split("""
@@ -81,14 +85,14 @@ elif format == 'wave':
     innercmd = shlex.split("""
         -background transparent -wave {wave_a}x{wave_l}
     """.format(
-        wave_a=1+round(factor*10),
-        wave_l=1+round(factor*200),
+        wave_a=1+doround(factor*10),
+        wave_l=1+doround(factor*200),
     ))
 
 elif format == 'swirl':
 
     factor = mindim/200.0
-    swirllen = 1+round(factor*40)
+    swirllen = 1+doround(factor*40)
 
     innercmd = shlex.split("""
         -background transparent -swirl {swirllen}
@@ -98,8 +102,8 @@ elif format == 'swirl':
 
 elif format == 'plasma':
 
-    blursize = round(mindim/100.0)
-    paintsize = 4+round((mindim/100.0)*2)
+    blursize = doround(mindim/100.0)
+    paintsize = 4+doround((mindim/100.0)*2)
 
     innercmd = shlex.split("""
         ( -size {mindim}x{mindim} tile:plasma:red-blue -write mpr:tmpplasma +delete
@@ -117,34 +121,63 @@ elif format == 'on-fire':
     factor = height/200.0
 
     values = dict(
-        blurlen0 = 2+round(factor*1.5),
+        blurlen0 = 2+doround(factor*1.5),
 
-        traillen1 = 1+round(factor*4),
-        blurlen1 = 1+round(factor*3),
-        wave1a = 1+round(factor*1.3),
-        wave1l = 1+round(factor*80),
+        traillen1 = 1+doround(factor*4),
+        blurlen1 = 1+doround(factor*3),
+        wave1a = 1+doround(factor*1.3),
+        wave1l = 1+doround(factor*80),
 
-        traillen2 = 1+round(factor*8),
-        wave2a = 1+round(factor*3),
-        wave2l = 1+round(factor*120),
+        traillen2 = 1+doround(factor*8),
+        wave2a = 1+doround(factor*3),
+        wave2l = 1+doround(factor*120),
 
         fac3pc = 25,
         fac3ipc = 400,
-        traillen3 = 1+round(factor*15/4.0),
-        rolllen3 = 1+round(factor*10/4.0),
-        wave3a = 1+round(factor*4),
-        wave3l = 1+round(factor*105),
+        traillen3 = 1+doround(factor*15/4.0),
+        rolllen3 = 1+doround(factor*10/4.0),
+        wave3a = 1+doround(factor*4),
+        wave3l = 1+doround(factor*105),
 
         fac4pc = 20,
         fac4ipc = 500,
-        traillen4 = 1+round(factor*25/5.0),
-        rolllen4 = 1+round(factor*25/5.0),
-        wave4a = 1+round(factor*5),
-        wave4l = 1+round(factor*150),
+        traillen4 = 1+doround(factor*25/5.0),
+        rolllen4 = 1+doround(factor*25/5.0),
+        wave4a = 1+doround(factor*5),
+        wave4l = 1+doround(factor*150),
+
+        fac5pc = 10,
+        fac5ipc = 1000,
+#        shiftuplen5 = 0,#1+doround(factor*50/10.0),
+        traillen5 = 1+doround(factor*60/10.0),
+        rolllen5 = 1+doround(factor*45/10.0),
+        wave5a = 1+doround(factor*6),
+        wave5l = 1+doround(factor*250),
     )
+    borderfactor = 1.5 # because blur dimensions are standard deviations, go further to
+                       # make sure eg. smoke is not truncated by too much
+    values.update(dict(
+        border = int( ( values['wave5a'] +
+                        values['wave4a'] +
+                        # |cos(100 deg)| ~~ 0.18
+                        values['wave3a'] + 0.18*values['traillen3']*values['fac3ipc']/100.0 +
+                        # |cos(110 deg)| ~~ 0.35
+                        values['wave2a'] + 0.35*values['traillen2'] +
+                        # |cos(120 deg)| = 0.5
+                        values['wave1a'] + 0.5*values['traillen1'] ) * borderfactor ) + 1 ,
+    ))
+    values.update(dict(
+        topborderadd = int( ( #values['shiftuplen5']*values['fac5ipc']/100.0 +
+                              values['traillen5']*values['fac5ipc']/100.0 +
+                              values['traillen4']*values['fac4ipc']/100.0 +
+                              values['traillen3']*values['fac3ipc']/100.0 +
+                              values['traillen2'] +
+                              values['traillen1'] +
+                              values['blurlen1']) * borderfactor ) + 1 - values['border'],
+    ))
 
     innercmd = shlex.split("""
-    -bordercolor none -background none -splice 0x50+0+0 -border 25
+    -bordercolor none -background none -splice 0x{topborderadd}+0+0 -border {border}
     -channel RGB +level-colors \#400000, -channel ALL
     \( +clone -channel RGB +level-colors \#a00000, -channel ALL -blur 0x{blurlen0} \)
     \( +clone -channel RGB +level-colors \#ff2000, -channel ALL -blur 0x{blurlen1} -motion-blur 0x{traillen1}+120
@@ -157,6 +190,10 @@ elif format == 'on-fire':
     \( +clone -modulate 115,65,103.0 -channel A +level 0%,75% -channel ALL
        -resize {fac4pc}% -motion-blur 0x{traillen4}+90 -resize {fac4ipc}%
        -rotate 90 -roll +{rolllen4}+0 -wave {wave4a},{wave4l} -roll -{rolllen4}+0 -rotate -90 \)
+    \( +clone -modulate 45,65,100.0 -channel A +level 0%,50% -channel ALL
+       -resize {fac5pc}% -motion-blur 0x{traillen5}+90 -resize {fac5ipc}% """ # -geometry +0-{shiftuplen5}
+    """
+       -rotate 90 -roll +{rolllen5}+0 -wave {wave5a},{wave5l} -roll -{rolllen5}+0 -rotate -90 \)
     -layers merge """.format(**values))
 
 else:
