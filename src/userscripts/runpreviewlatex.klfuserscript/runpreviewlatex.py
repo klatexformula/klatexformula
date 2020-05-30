1f83d4e89d138f1d1c1d9be48f10bc3e4eeac36a
Philippe Faist <phfaist@gmail.com>
2020-05-31 00:10:41 +0200

    preview border

--- src/userscripts/runpreviewlatex.klfuserscript/runpreviewlatex.py
+++ src/userscripts/runpreviewlatex.klfuserscript/runpreviewlatex.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-# runpdflatex.py
+# runpreviewlatex.py
 #   This file is part of the KLatexFormula Project.
-#   Copyright (C) 2018 by Philippe Faist
+#   Copyright (C) 2020 by Philippe Faist
 #   philippe.faist at bluewin.ch
 #
 #   This program is free software; you can redistribute it and/or modify
@@ -117,89 +117,126 @@ def run_cmd(do_cmd, ignore_fail=False, callargs={}):
 # add pdf page size commands latex
 # --------------------------------
 
-latexcode = ''
-with open(latexfname, 'r') as f:
-    latexcode = f.read()
+#latexcode = ''
+#with open(latexfname, 'r') as f:
+#    latexcode = f.read()
 
 # modify latex code
 
-m = re.search(r'\\begin\{document\}', latexcode)
-if m is None:
-    print("Latex code does not contain \\begin{document}!!")
-    raise ValueError("Latex code does not contain \\begin{document}!!")
-
-
-vertical_centering_arg = os.environ.get("KLF_ARG_vertical_centering")
-print("vertical_centering_arg = %r"%(vertical_centering_arg))
-
-crop_arg = os.environ.get("KLF_ARG_crop")
-print("crop_arg = %r"%(crop_arg))
-crop_arg_val = strtobool(crop_arg)
+# m = re.search(r'\\begin\{document\}', latexcode)
+# if m is None:
+#     print("Latex code does not contain \\begin{document}!!")
+#     raise ValueError("Latex code does not contain \\begin{document}!!")
 
 outlinefonts_val = strtobool(os.environ.get("KLF_SETTINGS_OUTLINEFONTS", "1"))
 
+fontsize_cmds = ""
+fsz = int(os.environ.get("KLF_INPUT_FONTSIZE", -1))
+if fsz != -1:
+    fontsize_cmds = r"""
+\fontsize{%(fsz)s}{%(fszbl)s}\selectfont%%
+"""[1:] % {
+        'fsz': fsz, 'fszbl': 1.2*fsz
+    }
 
-vspc = '0pt'
-if strtobool(vertical_centering_arg):
-    # plus 1fil: vertically center equations on page
-    vspc = '0pt plus 1fil'
-    
-if arg_ltxengine in ('pdflatex', 'xelatex'):
-    pagesizecmds = r"""
-\pdfpageheight=%(h)s
-\pdfpagewidth=%(w)s
-\paperwidth=%(w)s
-\paperheight=%(h)s
-"""[1:] % { 'w': ps_x, 'h': ps_y }
-elif arg_ltxengine == 'lualatex':
-    pagesizecmds = r"""
-\pageheight=%(h)s
-\pagewidth=%(w)s
-\paperwidth=%(w)s
-\paperheight=%(h)s
-"""[1:] % { 'w': ps_x, 'h': ps_y }
+rx_col = re.compile(r"rgba?\(\s*(?P<r>\d+)\s*,\s*(?P<g>\d+)\s*,\s*(?P<b>\d+)\s*(?:,\s*(?P<a>\d+)\s*)\)", flags=re.IGNORECASE)
+
+color_cmds = r"""
+\definecolor{klffgcolor}{RGB}{%(r)s,%(g)s,%(b)s}%%
+"""[1:] % rx_col.match(os.environ.get("KLF_INPUT_FG_COLOR_RGBA")).groupdict()
+
+### --- \pagecolor{} is ignored by {preview} package
+# hack_page_bg_color = ""
+# bgm = rx_col.match(os.environ.get("KLF_INPUT_BG_COLOR_RGBA")).groupdict()
+# if bgm['a'] and int(bgm['a']) > 0:
+#     hack_page_bg_color = r"""
+# \pdfliteral page{%
+#     q % gsave
+#     \current@page@color\GPT@space
+#     n % newpath
+#     0 0 \strip@pt\pdfpagewidth\GPT@space
+#     \strip@pt\pdfpageheight\GPT@space re % rectangle
+#     % there is no need to convert to bp
+#     f % fill
+#     Q% grestore
+#     }%
+# """[1:] ...........???????????
+#     color_cmds += r"""
+# \definecolor{klfbgcolor}{RGB}{%(r)s,%(g)s,%(b)s}%%
+# \pagecolor{klfbgcolor}%%
+# """[1:] % bgm.groupdict()
+
+m = re.match(r'(?P<top>[\d.+-]+)\s*,\s*(?P<right>[\d.+-]+)\s*,\s*(?P<bottom>[\d.+-]+)\s*,\s*(?P<left>[\d.+-]+)\s*(?:pt)?',
+             os.environ.get("KLF_SETTINGS_BORDEROFFSET", ""),
+             flags=re.IGNORECASE)
+preview_border_margins = ""
+if m is not None:
+    preview_border_margins = r"""
+\renewcommand{\PreviewBbAdjust}{-%(left)spt -%(bottom)spt %(right)spt %(top)spt}%%
+"""[1:] % m.groupdict()
+
+
+latex_input = os.environ.get("KLF_INPUT_LATEX")
+
+mm = os.environ.get("KLF_INPUT_MATHMODE")
+
+# replace display equations by inline \displaystyle equivalents:
+delims = mm.split('...', 1)
+def match_mathmode(a, b, mm, escape=True):
+    do_escape = re.escape if escape else lambda x : x
+    return re.match(r'\s*'+do_escape(a)+r'\s*\.\.\.\s*'+do_escape(b)+r'\s*', mm)
+
+def match_environ_rx(env, mm):
+    return match_mathmode( r'\\begin\s*\{(?P<envname>'+env+r')\}',
+                           r'\\end\s*\{(?P=envname)\}', mm,
+                           escape=False )
+
+if match_mathmode(r'\[', r'\]', mm) is not None or \
+   match_environ_rx(r'equation\*?', mm) is not None:
+    delims = (r'$\displaystyle', r'$')
 else:
-    raise ValueError("Unknown latex engine for paper size commands: {}".format(arg_ltxengine))
+    m = match_environ_rx(r'align\*?|gather\*?', mm)
+    if m is not None:
+        env = m.group('envname').rstrip('*')
+        delims = (r'$\displaystyle\begin{' + env + r'ed}',
+                  r'\end{' + env + r'ed}$')
+    else:
+        delims = mm.split('...', 1)
 
-LENGTHSCMDS = r"""
-\oddsidemargin=0pt
-\evensidemargin=0pt
-\topmargin=0pt
-\textwidth=%(w)s
-\textheight=%(h)s
-%(pagesizecmds)s
-\voffset=-1in
-\hoffset=-1in
-\headsep=0pt
-\headheight=0pt
-\marginparsep=0pt
-\footskip=0pt
+eqn_content = delims[0] + latex_input + delims[1]
 
-\parindent=0pt
-\parskip=0pt
 
-\def\klfXXXsetdisplayskiplengths{%%
-  \abovedisplayskip=%(vspc)s%%
-  \belowdisplayskip=%(vspc)s%%
-  \abovedisplayshortskip=%(vspc)s%%
-  \belowdisplayshortskip=%(vspc)s%%
+# --- form template ---
+
+latexcode = r"""
+\documentclass{article}
+\usepackage{color}
+
+%(preamble)s
+
+\usepackage[active,tightpage]{preview}
+%(preview_border_margins)s
+\begin{document}%%
+%(color_cmds)s%%
+%(fontsize_cmds)s%%
+\begin{preview}%%
+{\color{klffgcolor}%%
+%(eqn_content)s%%
+}%%
+\end{preview}
+\end{document}
+"""[1:] % {
+    'preamble': os.environ.get("KLF_INPUT_PREAMBLE"),
+    'preview_border_margins': preview_border_margins,
+    'color_cmds': color_cmds,
+    'fontsize_cmds': fontsize_cmds,
+    'eqn_content': eqn_content
 }
-\klfXXXsetdisplayskiplengths
-""" % { 'w': ps_x, 'h': ps_y, 'vspc': vspc, 'pagesizecmds': pagesizecmds }
 
-
-# m is regex match object for \begin{document}
-newlatexcode = latexcode[:m.start()]
-newlatexcode += LENGTHSCMDS
-newlatexcode += latexcode[m.start():m.end()]
-newlatexcode += "%\n\\klfXXXsetdisplayskiplengths\n"
-newlatexcode += latexcode[m.end():]
-
-print("NEW LATEX CODE:\n----------\n"+newlatexcode+"\n----------\n")
+print("LATEX CODE:\n----------\n"+latexcode+"\n----------\n")
 
 with open(latexfname, 'w') as f:
-    f.write(newlatexcode)
-
+    f.write(latexcode)
 
 
 #
@@ -218,7 +255,6 @@ run_cmd([ltxexe, os.path.basename(latexfname)])
 # crop and outline fonts if applicable
 # ------------------------------------
 
-crop_gs_args = []
 outlinefonts_gs_args = []
 
 if outlinefonts_val:
@@ -230,43 +266,6 @@ if outlinefonts_val:
     else:
         outlinefonts_gs_args = [ '-dNOCACHE' ]
 
-if crop_arg_val:
-
-    # get bounding box using gs
-    bboxoutput = run_cmd([
-        gsexe,
-        '-q',
-        '-dBATCH',
-        '-dNOPAUSE',
-        '-sDEVICE=bbox',
-        pdffname
-    ])
-
-    print("GS Bounding box info:\n" + bboxoutput + "\n\n")
-
-    mbb = re.search(
-        r'^%%HiResBoundingBox:\s+([0-9.e+-]+)\s+([0-9.e+-]+)\s+([0-9.e+-]+)\s+([0-9.e+-]+)\s*$',
-        bboxoutput,
-        flags=re.MULTILINE
-    )
-    if mbb is None:
-        raise ValueError("Ghostscript didn't report any bounding box!!")
-
-    bbox = (float(mbb.group(1)), float(mbb.group(2)), float(mbb.group(3)), float(mbb.group(4)))
-
-    crop_gs_args = [
-        '-dDEVICEWIDTHPOINTS='+str(intceil(bbox[2]-bbox[0])),
-        '-dDEVICEHEIGHTPOINTS='+str(intceil(bbox[3]-bbox[1])),
-        '-dFIXEDMEDIA',
-        '-c',
-        "<</BeginPage{%.3f %.3f translate 0 0 %.3f %.3f rectclip}>> setpagedevice "%(
-            -bbox[0],-bbox[1],bbox[2]-bbox[0], bbox[3]-bbox[1]),
-        '-f',
-        ]
-
-
-if outlinefonts_val or crop_arg_val:
-
     origpdffname = pdffname + '.orig.pdf'
     os.rename(pdffname, origpdffname)
 
@@ -277,7 +276,7 @@ if outlinefonts_val or crop_arg_val:
         '-dNOPAUSE',
         '-sDEVICE=pdfwrite',
         '-sOutputFile='+pdffname,
-    ] + outlinefonts_gs_args + crop_gs_args + [
+    ] + outlinefonts_gs_args + [
         origpdffname
     ])
 
